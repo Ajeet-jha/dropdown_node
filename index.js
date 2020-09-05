7b037536811af0b66228369e8b52ea1dd30c0cd4
Ajeet jha <jhaajeetcs@gmail.com>
2020-09-05 15:20:53 +0530

    added ajax

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/v1/countrylist', (req, res) => {
     res.send(data)
 })
 
+app.get('/ajax', (req, res) => {
+    res.send(req.query.data)
+})
+
 app.listen(4001,()=>{
     console.log("server started")
 });
